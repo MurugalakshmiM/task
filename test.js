c13a278439e272fb962d565df8150e5bfee67a77
MurugalakshmiM <priyareddy3298@outlook.com>
2019-03-20 14:55:26 +0530

    testcommit

---
+++ test.js
@@ -0,0 +1,21 @@
+/*s1 = "riyass";
+s2 = "riyass";
+i=0;
+count=0;
+while(s1[i]!=undefined || s2[i]!=undefined){
+if(s1[i]==s2[i]){
+count++;
+}
+i++;
+}
+if(i==count){
+console.log("equal");
+}else{
+console.log("notequal");
+}*/
+var d = 'Tom';
+function zeta() {
+    console.log(d);
+	// var d = "muruga"
+}
+zeta();
